e06b65a55ff1e9eb7038feef89d678d500a2f421
sssssnake <snakeappch@126.com>
2018-07-17 21:06:46 +0900

    debug

--- src/UCTSearch.cpp
+++ src/UCTSearch.cpp
@@ -1185,6 +1185,13 @@ int UCTSearch::shot(GameState& currstate, UCTNode* node, Random& rd, int buget,i
 				//node->m_children[child_in_round[tmpi]]->update_shot(np,nw);
 				//if(po_times!= nu)
 				//	myprintf("po_times %d,budgetUsed %d,playouts %d,after_po %d\n", po_times, nu, np, node->m_children[child_in_round[tmpi]]->shot_po_count);
+				if (isroot&& bestmove > -1)
+				{
+					std::string vertex;
+					vertex = m_rootstate.move_to_text(m_root->m_children[child_in_round[tmpi]]->get_move());
+					node->m_children[child_in_round[tmpi]]->get_move();
+					myprintf("move %s,np %d,nw %f,1.0*np - nw %f\n", vertex.c_str(),np, nw, 1.0*np - nw);
+				}
 				budgetUsed += nu;
 				playouts += np;
 				wins += nw;
