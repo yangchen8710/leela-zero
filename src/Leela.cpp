6e5eadbc391f3f964acac266456ad19695d7cdec
sssssnake <snakeappch@126.com>
2018-07-24 16:48:00 +0900

    1wv5w

--- src/Leela.cpp
+++ src/Leela.cpp
@@ -229,7 +229,7 @@ static void parse_commandline(int argc, char *argv[]) {
     if (vm.count("dumbpass")) {
         cfg_dumbpass = true;
     }
-	cfg_max_playouts = 5000;
+	cfg_max_playouts = 10000;
     if (vm.count("playouts")) {
         cfg_max_playouts = vm["playouts"].as<int>();
         if (!vm.count("noponder")) {
